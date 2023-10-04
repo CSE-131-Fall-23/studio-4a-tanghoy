e0f2e1f2d5740e6c011392e9d7f347513015931a
carolyntang <carolyntang@localhost>
2023-10-04 15:43:03 -0500

    complete

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,26 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenColor(StdDraw.MAGENTA);
+		StdDraw.filledRectangle(0.5, 0.5, 0.4, 0.2);
+		StdDraw.setPenColor(StdDraw.YELLOW);
+		double[] x = {0.1, 0.2, 0.3, 0.2};
+		double[] y = {0.6, 0.7, 0.6, 0.5};
+		
+		StdDraw.filledPolygon(x, y);
+		
+		StdDraw.setPenColor(StdDraw.PRINCETON_ORANGE);
+		StdDraw.filledCircle(0.7, 0.5, 0.15);
+		
+		StdDraw.setPenColor(StdDraw.GREEN);
+		double[] a = {0.75, 0.71, 0.75};
+		double[] b = {0.5, 0.5, 0.6};
+		StdDraw.filledPolygon(a, b);
+		
+		StdDraw.setPenColor(1, 50, 3);
+		double[] c = {0.7, 0.76, 0.77};
+		double[] d = {0.45, 0.51, 0.5};
+		StdDraw.filledPolygon(c, d);
+	
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,35 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType = in.nextLine();
+		int r;
+		int g;
+		int b;
+		double x;
+		double y;
+		double halfWidth;
+		double halfHeight;
+		
+		boolean isFilled = true;
+		
+		if (shapeType.equals("rectangle")) {
+			r = in.nextInt();
+			g = in.nextInt();
+			b = in.nextInt();
+			isFilled = in.nextBoolean();
+			
+			x = in.nextDouble();
+			y = in.nextDouble();
+			halfWidth = in.nextDouble();
+			halfHeight = in.nextDouble();
+			
+			if (isFilled = true) {
+				StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+			} else {
+				StdDraw.rectangle(x, y, halfWidth, halfHeight);
+			}
+			StdDraw.setPenColor(r,g,b);
+			StdDraw.rectangle (x, y, halfWidth, halfHeight);
+		}
 	}
 }
